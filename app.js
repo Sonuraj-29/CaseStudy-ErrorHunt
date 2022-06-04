de970e3cab4341337fc9ee3567ded66d0fbb1896
Sonuraj-29 <102570357+Sonuraj-29@users.noreply.github.com>
2022-06-04 21:44:16 +0530

    uri2

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/',function(req,res){
 });
 
 
-let port = process.env.MONGODB_URI;
+let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
   console.log("Server Ready on 5000");
